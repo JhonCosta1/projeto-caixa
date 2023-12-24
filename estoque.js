dcd2f7036d6e0aa9779ae219e5659894b47ed6a3
jhon costa <jhon.costa.dev@gmail.com>
2023-12-24 00:27:05 -0300

    feat(lógica): adicionado lógica para criar card em lista com localstorage
    criar card de produto em lista de acordo com localstorage
    
    BREAKING CHANGE: card

--- estoque.js
+++ estoque.js
@@ -19,3 +19,60 @@ class VoltarMenu{
     };
 }
 const voltarMenu = new VoltarMenu();
+class CriarCardProduto{
+    constructor() {
+        this.produtos = JSON.parse(localStorage.getItem("produtos")) || {};
+        this.card();
+    }
+
+    card() {
+        this.areaProdutos = document.querySelector("#area-produtos")
+        this.produtos.forEach(produto => {
+            this.divPrincipal = document.createElement("div");
+            this.divPrincipal.classList.add("produto");
+            this.ul = document.createElement("ul")
+            this.ul.classList.add("conteudo-produto");
+            this.liP = document.createElement("li")
+            this.liP.classList.add("dados-produto")
+            this.liP.innerHTML = `Nome: ${produto.nome}`
+            this.liS = document.createElement("li")
+            this.liS.classList.add("dados-produto")
+            this.liS.innerHTML = `Peso(Kg/L): ${produto.peso}`
+            this.liT = document.createElement("li")
+            this.liT.classList.add("dados-produto")
+            this.liT.innerHTML = `Validade: ${produto.validade}`
+            this.liQ = document.createElement("li")
+            this.liQ.classList.add("dados-produto")
+            this.liQ.innerHTML = `Quantidade: ${produto.quantidade}`
+            this.ul.appendChild(this.liP)
+            this.ul.appendChild(this.liS)
+            this.ul.appendChild(this.liT)
+            this.ul.appendChild(this.liQ)
+            this.divPrincipal.appendChild(this.ul);
+            this.areaProdutos.appendChild(this.divPrincipal);
+
+        });
+    }
+}
+const card = new CriarCardProduto();
+
+class ValidarUsuarioLogadoEstoqueV {
+    constructor() {
+        this.validarPermanencia();
+    }
+
+    redirecionar(direcao) {
+        let a = document.createElement("a")
+        a.href = direcao
+        a.target = "_self"
+        a.click()
+    };
+
+    validarPermanencia() {
+        if(localStorage.getItem('token') == null) {
+            alert('Você deve estar logado para acessar esta página!')
+            this.redirecionar("index.html");
+        };
+    };
+};
+const validarLogoutEstoqueV = new ValidarUsuarioLogadoEstoqueV();
